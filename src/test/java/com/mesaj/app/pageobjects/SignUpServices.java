dae0a90ea7dc7de98c7574e7337696b2ec1a7009
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-05-30 17:30:27 -0400

    Add Page Object Base class and use the waitFor bean that we created in DriverConfig class

--- src/test/java/com/mesaj/app/pageobjects/SignUpServices.java
+++ src/test/java/com/mesaj/app/pageobjects/SignUpServices.java
@@ -1,7 +1,9 @@
 package com.mesaj.app.pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,18 +13,18 @@ public class SignUpServices {
     @Autowired
     private SignUpPageObject signUpPageObject;
 
+    @Autowired
     private WebDriver driver;
 
     @Autowired
-    public SignUpServices(WebDriver driver) {
-        this.driver = driver;
-    }
+    WebDriverWait wait;
 
     public void go(String url) {
         this.driver.get(url);
     }
 
     public void writeFirstName(String firstName) {
+        this.wait.until(ExpectedConditions.visibilityOf(this.signUpPageObject.getFirstNameTextbox()));
         this.signUpPageObject.getFirstNameTextbox().sendKeys(firstName);
     }
 
