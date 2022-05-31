2935f8db00c445585d1a4b6f085f8ad88bfb678a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-05-31 00:51:21 -0400

    Add test databuilders

---
+++ src/test/java/com/mesaj/app/models/User.java
@@ -0,0 +1,23 @@
+package com.mesaj.app.models;
+
+import com.mesaj.app.enums.Gender;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Gender gender;
+    private String country;
+    private String birthDay;
+    private String birthMonth;
+    private String birthYear;
+    private String phone;
+    private String password;
+    private String passwordConfirmation;
+
+}
---
+++ src/test/java/com/mesaj/app/tasks/UserSignUp.java
@@ -0,0 +1,33 @@
+package com.mesaj.app.tasks;
+
+import com.mesaj.app.enums.Gender;
+import com.mesaj.app.models.User;
+import com.mesaj.app.pageobjects.SignUpServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSignUp {
+
+    @Autowired
+    private SignUpServices signUp;
+
+    public void withInfo(User user) {
+
+        signUp.writeFirstName(user.getFirstName());
+        signUp.writeLastName(user.getLastName());
+        signUp.writeEmail(user.getEmail());
+        signUp.selectGender(user.getGender());
+        //signUp.selectCountry(user.getCountry());
+        signUp.selectBirthDay(user.getBirthDay());
+        signUp.selectBirthMonth(user.getBirthMonth());
+        signUp.selectBirthYear(user.getBirthYear());
+        signUp.writePhone(user.getPhone());
+        signUp.writePassword(user.getPassword());
+        signUp.writeConfirmPassword(user.getPasswordConfirmation());
+
+        //signUp.clickOnSubmit();
+
+    }
+
+}
